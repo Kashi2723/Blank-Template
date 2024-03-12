a0ba469a7b25f03523b15c156e6af6cb8402928b
Kashish Shukla <kashishshukla46@gmail.com>
2024-03-12 20:46:02 +0530

    added utils files and .env

--- template.py
+++ template.py
@@ -16,10 +16,12 @@ list_of_files=[
     f"src/{package_name}/logger.py",
     f"src/{package_name}/exception.py",
     f"src/{package_name}/utils/__init__.py",
+    f"src/{package_name}/utils/utils.py",
     "notebooks/research.ipynb",
     "notebooks/data/.gitkeep",
     "requirements.txt",
     "setup.py",
+    '.env',
     "init_setup.sh",
 ]
 
